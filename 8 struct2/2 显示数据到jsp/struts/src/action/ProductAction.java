1f6a2de4de858ba48ef44301a7cccd793f00949a
zkxshg <zkxshg@126.com>
2022-09-30 21:41:35 +0800

    Add files via upload

---
+++ 8 struct2/2 显示数据到jsp/struts/src/action/ProductAction.java
@@ -0,0 +1,21 @@
+package action;
+
+import bean.Product;
+
+public class ProductAction {
+	private Product product;
+
+	public String show() {
+		product = new Product();
+		product.setName("iphone7");
+		return "show";
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+}
