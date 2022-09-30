788f6aacfccae2f6d739235570337189bf09e841
zkxshg <zkxshg@126.com>
2022-09-30 19:09:13 +0800

    Add files via upload

---
+++ 1 servlet/12 JSON/getOne/j2ee/src/GetOneServlet.java
@@ -0,0 +1,27 @@
+  
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;  
+  
+public class GetOneServlet extends HttpServlet {  
+    protected void service(HttpServletRequest request, HttpServletResponse response)  
+            throws ServletException, IOException {
+    	 
+    	Hero hero = new Hero();
+    	hero.setName("¸ÇÂ×");
+    	hero.setHp(353);
+    	
+    	JSONObject json= new JSONObject();
+  
+    	json.put("hero", JSONObject.fromObject(hero));
+    	response.setContentType("text/html;charset=utf-8");  
+    	response.getWriter().print(json);
+    }  
+}  
